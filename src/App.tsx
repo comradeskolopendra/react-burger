d447458d08b05c7669948eb59ef7837a5fa5840e
DevDima <DCHukhin@logycom.kz>
2023-08-25 13:50:54 +0600

    init sprint step 1 branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
+
 function App() {
     return (
-        <section>
-            Я проживаю по адресу:
-            {renderAddress("Пушкина", "Колотушкина")}
-        </section>
-    );
-}
-
-const renderAddress = (street: string, house: string) => {
-    return (
         <div>
-            <p>Улица: {street}</p>
-            <p>Дом: {house}</p>
+            123
         </div>
     );
-};
+}
 
 export default App;
