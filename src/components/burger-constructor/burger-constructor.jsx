57eedb34c45d39d4c0d50dd54495d161c840ea33
comradeskolopendra <comradeskolopendra@gmail.com>
2023-08-27 20:52:52 +0600

    updatrer burger-constructor component

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,39 +1,60 @@
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement, Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./burger-constructor.module.css";
+import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerConstructor = ({ ingredients }) => {
     return (
-        <section className={`${styles.wrapper} pb-10`}>
-            <ConstructorElement
-                type="top"
-                isLocked={true}
-                price={200}
-                text="Краторная булка N-200i (верх)"
-                thumbnail="https://code.s3.yandex.net/react/code/bun-02.png"
-            />
+        <div className={styles.wrapper}>
+            <section className={`${styles.constructor} pb-10 pr-4`}>
+                <ConstructorElement
+                    type="top"
+                    isLocked={true}
+                    price={200}
+                    text="Краторная булка N-200i (верх)"
+                    thumbnail="https://code.s3.yandex.net/react/code/bun-02.png"
+                    extraClass="ml-8"
+                />
 
-            <div className={styles.ingredientsWrapper}>
-                {
-                    ingredients.map((ingredient) => (
-                        <ConstructorElement
-                            isLocked={false}
-                            price={ingredient.price}
-                            thumbnail={ingredient.image}
-                            text={ingredient.name}
-                        />
-                    ))
-                }
-            </div>
+                <ul className={`${styles.ingredientsWrapper}`}>
+                    {
+                        ingredients.map((ingredient) => (
+                            <li style={{ display: "flex", alignItems: "center" }}>
+                                <div className="mr-2">
+                                    <DragIcon />
+                                </div>
+                                <ConstructorElement
+                                    isLocked={false}
+                                    price={ingredient.price}
+                                    thumbnail={ingredient.image}
+                                    text={ingredient.name}
+                                />
+                            </li>
+                        ))
+                    }
+                </ul>
 
-            <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                price={200}
-                text="Краторная булка N-200i (низ)"
-                thumbnail="https://code.s3.yandex.net/react/code/bun-02.png"
-            />
-        </section>
+                <ConstructorElement
+                    type="bottom"
+                    isLocked={true}
+                    price={200}
+                    text="Краторная булка N-200i (низ)"
+                    thumbnail="https://code.s3.yandex.net/react/code/bun-02.png"
+                    extraClass="ml-8"
+                />
+            </section>
+
+            <section className={styles.orderBlock}>
+                <div className={styles.price}>
+                    <p className="mr-2 text text_type_digits-medium">123</p>
+                    <CurrencyIcon type="primary" />
+                </div>
+
+                <Button htmlType="button" type="primary" size="large">
+                    Оформить заказ
+                </Button>
+            </section>
+        </div>
     );
 };
 
