5499f4e47d23deb2fbb7b79a47ab9cee924ac4cd
DevDima <DCHukhin@logycom.kz>
2023-09-01 11:03:57 +0600

    init step-2

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,30 +1,77 @@
 import AppHeader from "../app-header/app-header";
-import { data } from "../../utils/data";
+import { URL_API } from "../../utils/data";
 
 import "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./app.module.css";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
+import { useEffect, useState } from "react";
 
 function App() {
-    const buns = data.filter((element) => element.type === "bun");
-    const sauces = data.filter((element) => element.type === "sauce");
-    const mains = data.filter((element) => element.type === "main");
+    const [ingredients, setIngredients] = useState({
+        hasError: false,
+        data: [],
+        isLoading: false,
+    });
+
+    const getIngredients = async () => {
+        setIngredients({ ...ingredients, isLoading: true });
+        console.log(ingredients);
+        try {
+            const response = await fetch(URL_API);
+
+            if (!response.ok) throw new Error("fetch error");
+
+            const { data } = await response.json();
+            console.log(data, response.ok);
+
+            setIngredients({ ...ingredients, data });
+        } catch (error) {
+            setIngredients({ ...ingredients, hasError: true });
+            console.error(error);
+        } finally {
+            setIngredients({ ...ingredients, isLoading: false });
+        }
+    };
+
+    useEffect(() => {
+        getIngredients();
+    }, []);
+
+    useEffect(() => {
+        console.log(ingredients)
+    }, [ingredients]);
+
+    const buns = ingredients.data.filter((element) => element.type === "bun");
+    const sauces = ingredients.data.filter(
+        (element) => element.type === "sauce"
+    );
+    const mains = ingredients.data.filter((element) => element.type === "main");
     return (
         <div>
             <AppHeader />
-            <main className={styles.main}>
-                <h1 className="text text_type_main-large mt-10 mb-5">
-                    Соберите бургер
-                </h1>
+            {ingredients.isLoading && "Идет загрузка"}
+            {ingredients.hasError && "Что-то пошло не так!"}
+            {ingredients.data.length !== 0 &&
+                !ingredients.hasError &&
+                !ingredients.isLoading && (
+                    <main className={styles.main}>
+                        <h1 className="text text_type_main-large mt-10 mb-5">
+                            Соберите бургер
+                        </h1>
 
-                <div className={styles.wrapper}>
-                    <BurgerIngredients buns={buns} sauces={sauces} mains={mains} />
-                    <BurgerConstructor
-                        ingredients={[...mains, ...sauces]}
-                    />
-                </div>
-            </main>
+                        <div className={styles.wrapper}>
+                            <BurgerIngredients
+                                buns={buns}
+                                sauces={sauces}
+                                mains={mains}
+                            />
+                            <BurgerConstructor
+                                ingredients={[...mains, ...sauces]}
+                            />
+                        </div>
+                    </main>
+                )}
         </div>
     );
 }
