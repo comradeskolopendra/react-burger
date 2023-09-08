b20afe09a2ea054c975928b5d789f40127f222a6
DevDima <DCHukhin@logycom.kz>
2023-09-08 15:05:52 +0600

    fix errors with api

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,15 +1,16 @@
 import { BASE_URL } from "../utils/constants";
 
+const checkResponse = (res) =>
+    res.ok ? res.json() : new Error(`Ошибка: ${res.status}`);
+
+const request = (url, options = {}) => fetch(url, options).then(checkResponse);
+
 const getIngredients = async (updateState) => {
     updateState((prevState) => ({ ...prevState, isLoading: true }));
     try {
-        const response = await fetch(`${BASE_URL}/ingredients`);
-
-        if (!response.ok) throw new Error("fetch get ingredients error");
-
-        const { data } = await response.json();
-
+        const { data } = await request(`${BASE_URL}/ingredients`);
         updateState((prevState) => ({ ...prevState, data }));
+        console.log(data);
     } catch (error) {
         updateState((prevState) => ({ ...prevState, hasError: true }));
         console.error(error);
@@ -20,21 +21,15 @@ const getIngredients = async (updateState) => {
 
 const createOrder = async (updateState, ingredientsIds) => {
     updateState((prevState) => ({ ...prevState, isLoading: true }));
-
     try {
-        console.log(ingredientsIds)
-        const response = await fetch(`${BASE_URL}/orders`, {
+        console.log(ingredientsIds);
+        const data = await request(`${BASE_URL}/orders`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
             },
-            body: JSON.stringify({ ingredients: ingredientsIds })
+            body: JSON.stringify({ ingredients: ingredientsIds }),
         });
-
-        if (!response.ok) throw new Error("fetch create order error");
-
-        const data = await response.json();
-
         updateState((prevState) => ({ ...prevState, data }));
     } catch (error) {
         updateState((prevState) => ({ ...prevState, hasError: true }));
